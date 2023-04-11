c4d8b10e6768f2b9d4953c1ddd4ea7de5258c308
Alexon7 <alenkaalexon@gmail.com>
2023-04-11 12:59:31 +0200

    add main scss

--- src/index.js
+++ src/index.js
@@ -1,2 +1,1 @@
 import './js/api-service';
-import './js/api-service';
--- src/js/api-service.js
+++ src/js/api-service.js
@@ -2,4 +2,9 @@ import axios from 'axios';
 
 export class Books {
   #BASE_URL = 'https://books-backend.p.goit.global/books';
+  #categoryId = '';
+
+  fetchCategories(page) {
+    return axios.get('https://books-backend.p.goit.global/books/category-list');
+  }
 }
