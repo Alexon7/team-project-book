16a3d8f538b85847a31dc753dab4320846345d5c
Alexon7 <alenkaalexon@gmail.com>
2023-04-11 15:10:49 +0200

    header

--- src/js/api-service.js
+++ src/js/api-service.js
@@ -1,10 +1,1 @@
 import axios from 'axios';
-
-export class Books {
-  #BASE_URL = 'https://books-backend.p.goit.global/books';
-  #categoryId = '';
-
-  fetchCategories(page) {
-    return axios.get('https://books-backend.p.goit.global/books/category-list');
-  }
-}
