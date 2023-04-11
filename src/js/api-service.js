155f5985b1f9446346f47934e43b06a5ab2af9a5
Kralinskyi Ivan <i.kralinski@gmail.com>
2023-04-11 17:14:17 +0300

    Iskra made APi

--- src/js/api-service.js
+++ src/js/api-service.js
@@ -1,10 +1,37 @@
 import axios from 'axios';
 
-export class Books {
-  #BASE_URL = 'https://books-backend.p.goit.global/books';
-  #categoryId = '';
+/*
+Перелік категорій книг__________________________https://books-backend.p.goit.global/books/category-list 
+Популярні книги, що належать до усіх категорій__https://books-backend.p.goit.global/books/top-books 
+Книги окремої категорії_________________________https://books-backend.p.goit.global/books/category?category=selectedCategory
+Детальна інформація про книгу___________________https://books-backend.p.goit.global/books/ bookId
+*/
 
-  fetchCategories(page) {
-    return axios.get('https://books-backend.p.goit.global/books/category-list');
+const instance = axios.create({
+  baseURL: 'https://books-backend.p.goit.global/books/',
+});
+
+page = 0;
+query = null;
+
+export class BookAPI {
+  getCategories() {
+    return instance.get('category-list');
+  }
+
+  getBooksByCategories(selectedCategory) {
+    return instance.get('category', {
+      params: {
+        category: selectedCategory,
+      },
+    });
+  }
+
+  getBooksTop() {
+    return instance.get('top-books');
+  }
+
+  getBooksById(bookId) {
+    return instance.get(`${bookId}`);
   }
 }
