44ab5ad2799aaf15ed56aefb1c815622da1326c8
Kralinskyi Ivan <i.kralinski@gmail.com>
2023-04-11 17:13:42 +0300

    Iskra made APi

--- src/js/kralinskyi.js
+++ src/js/kralinskyi.js
@@ -1,46 +1,37 @@
 import axios from 'axios';
 
-/**
- * 
-Перелік категорій книг	https://books-backend.p.goit.global/books/category-list 
-
-Популярні книги, що належать до усіх категорій	https://books-backend.p.goit.global/books/top-books 
-
-Книги окремої категорії	https://books-backend.p.goit.global/books/category?category=selectedCategory
-
-Детальна інформація про книгу	https://books-backend.p.goit.global/books/ bookId
- */
+/*
+Перелік категорій книг__________________________https://books-backend.p.goit.global/books/category-list 
+Популярні книги, що належать до усіх категорій__https://books-backend.p.goit.global/books/top-books 
+Книги окремої категорії_________________________https://books-backend.p.goit.global/books/category?category=selectedCategory
+Детальна інформація про книгу___________________https://books-backend.p.goit.global/books/ bookId
+*/
 
 const instance = axios.create({
   baseURL: 'https://books-backend.p.goit.global/books/',
 });
 
-function fetchTopBooks() {
-  return instance.get('top-books');
+page = 0;
+query = null;
+
+export class BookAPI {
+  getCategories() {
+    return instance.get('category-list');
+  }
+
+  getBooksByCategories(selectedCategory) {
+    return instance.get('category', {
+      params: {
+        category: selectedCategory,
+      },
+    });
+  }
+
+  getBooksTop() {
+    return instance.get('top-books');
+  }
+
+  getBooksById(bookId) {
+    return instance.get(`${bookId}`);
+  }
 }
-
-// fetchTopBooks();
-
-function fetchCategories() {
-  return instance.get('category-list');
-}
-
-// fetchCategories();
-
-function fetchSelectedCategory(selectedCategory) {
-  return instance.get('category', {
-    params: {
-      category: selectedCategory,
-    },
-  });
-}
-
-// fetchSelectedCategory('Picture Books');
-
-function fetchBookInfoById(bookId) {
-  return instance.get(`_id=${bookId}`);
-}
-
-// What to do?
-
-// fetchBookInfoById('642fd89ac8cf5ee957f12361');
