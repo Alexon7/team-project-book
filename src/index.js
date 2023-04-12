8ba998f12588dc36db2b90ce69e1cb34dc18c57c
Irina Iskra <iskrairina.a@gmail.com>
2023-04-13 01:33:35 +0300

    book Id and modal

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './js/api-service';
 import './js/home-page';
 import './js/kralinskyi';
-import './js/render-bestsellers-bookByType';
+import './js/render-bookByType';
 import './js/auth';
 import './js/topic';
 import './js/render-categories-list';
+import './js/show-modal-about-book';
+import './js/modal';
--- src/js/render-bookByType.js
+++ src/js/render-bookByType.js
@@ -1,19 +1,18 @@
 // Iskra Matjuha
 import { BookAPI } from './api-service';
+
 const categoryBooksEl = document.querySelector('.book-category__list');
 const bookApi = new BookAPI();
 //запрос книг по выбранной категории - считываем категорию со списка категорий - и нужно прорисовать книги из нее
 export async function handleRenderCategoryItem(category) {
-  console.log(category);
-
   const response = await bookApi.getBooksByCategories(category);
   const categoryBooks = response.data;
-  console.log(response);
+  // console.log(response);
   console.log(categoryBooks);
-  categoryBooksEl.innerHTML = `
+  categoryBooksEl.innerHTML = `<h2>${category}</h2>
       ${categoryBooks
         .map(
-          el => `<li class="category-books" > <a href= "" class ="book"> <img src = ${el.book_image} data-id= ${el._id}> </a>
+          el => `<li class="category-books" > <a href= "" class ="book"> <img src = ${el.book_image} data-id= ${el._id}> <h3> ${el.title}</h3> <p>${el.author} </p></a>
    </li>`
         )
         .join('')}
