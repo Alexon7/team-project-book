946478e0e68bb66965aac715ff0edc2bd098d112
Irina Iskra <iskrairina.a@gmail.com>
2023-04-12 16:52:06 +0300

    fix category

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import './js/api-service';
 import './js/home-page';
 import './js/kralinskyi';
 import './js/render-categories-list';
-import './js/auth'
+import './js/auth';
--- src/js/render-bestsellers-bookByType.js
+++ src/js/render-bestsellers-bookByType.js
@@ -1,5 +1,6 @@
+// Iskra Matjuha
 import { BookAPI } from './api-service';
-
+const categoryBooksEl = document.querySelector('.book-category__list');
 const bookApi = new BookAPI();
 //запрос книг по выбранной категории - считываем категорию со списка категорий - и нужно прорисовать книги из нее
 export async function handleRenderCategoryItem(category) {
@@ -9,19 +10,12 @@ export async function handleRenderCategoryItem(category) {
   const categoryBooks = response.data;
   console.log(response);
   console.log(categoryBooks);
-  // try {
-  //       console.log(categoryBooks);
-  //       categoryBooksEl.innerHTML = `
-  //     ${categoryBooks
-  //       .map(
-  //         el => `<li class="category-books" > <a href= "" class ="book"> <img src = ${el.book_image} data-id= ${el._id}> </a>
-  //  </li>`
-  //       )
-  //       .join('')}
-  //     `;
-  //     }
-  //     console.log('Категория: ', category); //надо будет вставить ссылку  на отрисовку топов
-  //   } catch (error) {
-  //     console.log(error.message);
-  //   }
+  categoryBooksEl.innerHTML = `
+      ${categoryBooks
+        .map(
+          el => `<li class="category-books" > <a href= "" class ="book"> <img src = ${el.book_image} data-id= ${el._id}> </a>
+   </li>`
+        )
+        .join('')}
+      `;
 }
