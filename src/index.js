c111a459a0d7057ab02a12b43cb0ab0de6d1145a
Alexon7 <alenkaalexon@gmail.com>
2023-04-12 16:43:51 +0200

    add topic html and js

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import './js/api-service';
 import './js/home-page';
 import './js/kralinskyi';
 import './js/iskra';
-import './js/auth'
+import './js/auth';
+import './js/topic';
---
+++ src/js/topic.js
@@ -0,0 +1,31 @@
+// Alenka Alexon
+
+const body = document.querySelector('body');
+const checkBoxEl = document.querySelector(
+  '.theme-switch .theme-switch__toggle'
+);
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const checkTheme = localStorage.getItem('theme');
+
+body.classList.add(Theme.LIGHT);
+
+if (checkTheme === Theme.DARK) {
+  body.classList.replace(Theme.LIGHT, Theme.DARK);
+  checkBoxEl.setAttribute('checked', 'checked');
+}
+
+function repleceCheckTheme() {
+  if (body.classList.contains(Theme.LIGHT)) {
+    body.classList.replace(Theme.LIGHT, Theme.DARK);
+    localStorage.setItem('theme', body.getAttribute('class'));
+  } else {
+    body.classList.replace(Theme.DARK, Theme.LIGHT);
+    localStorage.setItem('theme', body.getAttribute('class'));
+  }
+}
+checkBoxEl.addEventListener('click', repleceCheckTheme);
