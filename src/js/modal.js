1919be1dd47f66e899e56a5b2a2a0c660babaa54
Ront31 <kolya.maslov.09@gmail.com>
2023-04-13 11:00:35 +0300

    Maslov modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -3,26 +3,29 @@
 
 // import 'basiclightbox/dist/basiclightbox.min.css';
 
-// export const modal = async content => {
-//   const modal = basicLightbox.create(content, {
-//     onShow: instance => {
-//       instance.element().querySelector('.close-icon').onclick = instance.close;
-//     },
-//     onClose: () => {
-//       localStorage.removeItem('openInfoBook');
-//       document.body.classList.remove('stop-scroll');
-//     },
-//   });
-//   modal.show();
-//   window.addEventListener('keydown', closeModal);
+const basicLightbox = require('basiclightbox');
+require('basiclightbox/dist/basiclightbox.min.css');
 
-//   function closeModal(event) {
-//     if (event.code === 'Escape') {
-//       modal.close();
-//       window.removeEventListener('keydown', closeModal);
-//     }
-//   }
-// };
+export const modal = async content => {
+  const modal = basicLightbox.create(content, {
+    onShow: instance => {
+      instance.element().querySelector('.close-icon').onclick = instance.close;
+    },
+    onClose: () => {
+      localStorage.removeItem('openInfoBook');
+      document.body.classList.remove('stop-scroll');
+    },
+  });
+  modal.show();
+  window.addEventListener('keydown', closeModal);
+
+  function closeModal(event) {
+    if (event.code === 'Escape') {
+      modal.close();
+      window.removeEventListener('keydown', closeModal);
+    }
+  }
+};
 
 // чат gpt посоветовал:
 /*
