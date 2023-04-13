0952afc697ac146ed82486a0992e58d92c75f6da
Irina Iskra <iskrairina.a@gmail.com>
2023-04-13 18:07:06 +0300

    update bookid

--- src/js/show-modal-about-book.js
+++ src/js/show-modal-about-book.js
@@ -3,8 +3,8 @@ import { modal } from './modal.js';
 import { BookAPI } from './api-service.js';
 const bookApi = new BookAPI();
 
-const categoryBooksEl = document.querySelector('.books-of-category__list');
-categoryBooksEl.addEventListener('click', handleDataBookById);
+// const categoryBooksEl = document.querySelector('.books-of-category__list');
+// categoryBooksEl.addEventListener('click', handleDataBookById);
 
 // єтот кусок не надо - id книги получаем по клику книги:
 // const booksId = async bookId => {
@@ -35,17 +35,4 @@ export function show(event) {
         showBookModal(event.target.parentNode.dataset.id);
     });
   });
-}
-
-// при клике на книгу считываем id и рендерим модалку - потом с ней работаем. наверное надо разенсти по разным файлам
-function handleDataBookById(event) {
-  event.preventDefault();
-  if (event.target.nodeName !== 'IMG') {
-    return;
-  }
-
-  const bookId = event.target.dataset.id;
-  console.log(bookId);
-
-  showBookModal(bookId);
 }
