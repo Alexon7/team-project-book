7a99e67f48563a3ef6c18f5557881ae2c2b33260
Kralinskyi Ivan <i.kralinski@gmail.com>
2023-04-13 10:36:11 +0300

    comment modal.js

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,25 +1,33 @@
 // Здесь жесть
-import * as basicLightbox from 'basiclightbox';
+// import * as basicLightbox from 'basiclightbox';
 
-import 'basiclightbox/dist/basiclightbox.min.css';
+// import 'basiclightbox/dist/basiclightbox.min.css';
 
-export const modal = async content => {
-  const modal = basicLightbox.create(content, {
-    onShow: instance => {
-      instance.element().querySelector('.close-icon').onclick = instance.close;
-    },
-    onClose: () => {
-      localStorage.removeItem('openInfoBook');
-      document.body.classList.remove('stop-scroll');
-    },
-  });
-  modal.show();
-  window.addEventListener('keydown', closeModal);
+// export const modal = async content => {
+//   const modal = basicLightbox.create(content, {
+//     onShow: instance => {
+//       instance.element().querySelector('.close-icon').onclick = instance.close;
+//     },
+//     onClose: () => {
+//       localStorage.removeItem('openInfoBook');
+//       document.body.classList.remove('stop-scroll');
+//     },
+//   });
+//   modal.show();
+//   window.addEventListener('keydown', closeModal);
 
-  function closeModal(event) {
-    if (event.code === 'Escape') {
-      modal.close();
-      window.removeEventListener('keydown', closeModal);
-    }
-  }
-};
+//   function closeModal(event) {
+//     if (event.code === 'Escape') {
+//       modal.close();
+//       window.removeEventListener('keydown', closeModal);
+//     }
+//   }
+// };
+
+// чат gpt посоветовал:
+/*
+const basicLightbox = require('basiclightbox');
+require('basiclightbox/dist/basiclightbox.min.css');
+
+или юзать что-то другое
+*/
