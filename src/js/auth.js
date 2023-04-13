dbcef57cf1d4bf81f798932fd05cf089c9aab221
Anton Nikolaenko <anton.ck.ua@gmail.com>
2023-04-13 09:41:19 +0300

    fix

--- src/js/auth.js
+++ src/js/auth.js
@@ -8,15 +8,16 @@ import {
   signInWithEmailAndPassword,
 } from 'firebase/auth';
 
-export const txtEmail = document.querySelector('#user_email');
-export const txtPassword = document.querySelector('#user_password');
+console.log(AuthErrorCodes);
+const userEmail = document.querySelector('#user_email');
+const userPassword = document.querySelector('#user_password');
 
-// export const btnLogin = document.querySelector('#btnLogin');
-export const btnSignup = document.querySelector('#btnSignup');
+const btnLogin = document.querySelector('#btnLogin');
+const btnSignup = document.querySelector('#btnSignup');
 
-// export const btnLogout = document.querySelector('#btnLogout');
+const btnLogout = document.querySelector('#btnLogout');
 
-// export const divAuthState = document.querySelector('#divAuthState');
+const messageLogin = document.querySelector('.message__login');
 // export const lblAuthState = document.querySelector('#lblAuthState');
 
 // export const divLoginError = document.querySelector('#divLoginError');
@@ -34,99 +35,108 @@ const firebaseSettings = initializeApp({
   appId: '1:560994919300:web:10cdf4110616a9d01f33d1',
 });
 const auth = getAuth(firebaseSettings);
-
+console.log(auth);
 // Create new account using email/password
 const createAccount = async event => {
   event.preventDefault();
-  const email = user_email.value.trim();
-  const password = user_password.value.trim();
+  const email = userEmail.value.trim();
+  const password = userPassword.value.trim();
 
   try {
-    await createUserWithEmailAndPassword(auth, email, password);
+    await createUserWithEmailAndPassword(auth, email, password).then(res => {
+      console.log(res);
+      console.log(res.email);
+    });
+  } catch (error) {
+    console.log(`There was an error: ${error}`);
+    showLoginError(error);
+    console.log(showLoginError(error));
+  }
+};
+
+btnSignup.addEventListener('click', createAccount);
+
+const showLoginForm = () => {
+  userEmail.style.display = 'block';
+  userPassword.style.display = 'block';
+};
+
+const showApp = () => {
+  userEmail.style.display = 'block';
+  userPassword.style.display = 'block';
+};
+
+//  const hideLoginError = () => {
+//   divLoginError.style.display = 'none';
+//   lblLoginErrorMessage.innerHTML = '';
+// };
+
+const showLoginError = error => {
+  if (error.code == AuthErrorCodes.INVALID_PASSWORD) {
+    alert('Wrong password. Try again.');
+  } else {
+    alert(`Error: ${error.message}`);
+  }
+};
+
+const showLoginState = user => {
+  console.log(
+    // `You're logged in as ${user.displayName} (uid: ${user.uid}, email: ${user.email}) `
+    `You're logged in as  email: ${user.email}) `
+  );
+  messageLogin.innerHTML = `You're logged in`;
+};
+
+// hideLoginError();
+
+// Login using email/password
+const loginEmailPassword = async e => {
+  e.preventDefault();
+
+  const loginEmail = userEmail.value.trim();
+  const loginPassword = userPassword.value.trim();
+
+  // step 1: try doing this w/o error handling, and then add try/catch
+  // await signInWithEmailAndPassword(auth, loginEmail, loginPassword);
+
+  // step 2: add error handling
+  try {
+    await signInWithEmailAndPassword(auth, loginEmail, loginPassword);
   } catch (error) {
     console.log(`There was an error: ${error}`);
     showLoginError(error);
   }
 };
 
-btnSignup.addEventListener('click', createAccount);
+// Monitor auth state
+const monitorAuthState = async () => {
+  onAuthStateChanged(auth, user => {
+    if (user) {
+      console.log(user);
+      console.log(auth);
+      // showApp();
+      showLoginState(user);
 
-
-const showLoginForm = () => {
-  // login.style.display = 'block';
-  // app.style.display = 'none';
+      // hideLoginError();
+      // hideLinkError();
+    } else {
+      showLoginForm();
+      messageLogin.innerHTML = '';
+      messageLogin.insertAdjacentHTML(
+        'beforeend',
+        `<p>You're not logged in.</p>`
+      );
+    }
+  });
 };
 
-// export const showApp = () => {
-//   login.style.display = 'none';
-//   app.style.display = 'block';
-// };
-
-// export const hideLoginError = () => {
-//   divLoginError.style.display = 'none';
-//   lblLoginErrorMessage.innerHTML = '';
-// };
-
-// export const showLoginError = error => {
-//   if (error.code == AuthErrorCodes.INVALID_PASSWORD) {
-//     alert('Wrong password. Try again.');
-//   } else {
-//     alert(`Error: ${error.message}`);
-//   }
-// };
-
-// export const showLoginState = user => {
-//   alert(`You're logged in as ${user.displayName} (uid: ${user.uid}, email: ${user.email}) `);
-// };
-
-// hideLoginError();
-
-
-
-// Login using email/password
-// const loginEmailPassword = async () => {
-//   const loginEmail = user_email.value.trim();
-//   const loginPassword = user_password.value.trim();
-
-  // step 1: try doing this w/o error handling, and then add try/catch
-  // await signInWithEmailAndPassword(auth, loginEmail, loginPassword);
-
-  // step 2: add error handling
-  // try {
-  //   await signInWithEmailAndPassword(auth, loginEmail, loginPassword)
-  // }
-  // catch(error) {
-  //   console.log(`There was an error: ${error}`)
-  //   showLoginError(error)
-  // }
-// };
-
-
-
-// Monitor auth state
-// const monitorAuthState = async () => {
-//   onAuthStateChanged(auth, user => {
-//     if (user) {
-//       console.log(user);
-//       showApp();
-//       showLoginState(user);
-
-//       // hideLoginError();
-//       hideLinkError();
-//     } else {
-//       showLoginForm();
-//       // lblAuthState.innerHTML = `You're not logged in.`;
-//       alert(`You're not logged in.`);
-//     }
-//   });
-// };
-
 // Log out
-// const logout = async () => {
-//   await signOut(auth);
-// };
+const logout = async () => {
+  await signOut(auth);
+};
 
-// btnLogin.addEventListener('click', loginEmailPassword);
-// btnLogout.addEventListener('click', logout);
+btnLogin.addEventListener('click', loginEmailPassword);
+btnLogout.addEventListener('click', logout);
 
-// monitorAuthState();
+monitorAuthState();
+// console.log(auth);
