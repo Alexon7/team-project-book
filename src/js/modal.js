50cca0cfd7eeebfb6f34d7bf0ca38ab685409793
Kralinskyi Ivan <i.kralinski@gmail.com>
2023-04-13 10:23:49 +0300

    uncoment

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,23 +1,23 @@
-// import * as basicLightbox from 'basiclightbox';
-// import 'basiclightbox/dist/basiclightbox.min.css';
+import * as basicLightbox from 'basiclightbox';
+import 'basiclightbox/dist/basiclightbox.min.css';
 
-// export const modal = async content => {
-//   const modal = basicLightbox.create(content, {
-//     onShow: instance => {
-//       instance.element().querySelector('.close-icon').onclick = instance.close;
-//     },
-//     onClose: () => {
-//       localStorage.removeItem('openInfoBook');
-//       document.body.classList.remove('stop-scroll');
-//     },
-//   });
-//   modal.show();
-//   window.addEventListener('keydown', closeModal);
+export const modal = async content => {
+  const modal = basicLightbox.create(content, {
+    onShow: instance => {
+      instance.element().querySelector('.close-icon').onclick = instance.close;
+    },
+    onClose: () => {
+      localStorage.removeItem('openInfoBook');
+      document.body.classList.remove('stop-scroll');
+    },
+  });
+  modal.show();
+  window.addEventListener('keydown', closeModal);
 
-//   function closeModal(event) {
-//     if (event.code === 'Escape') {
-//       modal.close();
-//       window.removeEventListener('keydown', closeModal);
-//     }
-//   }
-// };
+  function closeModal(event) {
+    if (event.code === 'Escape') {
+      modal.close();
+      window.removeEventListener('keydown', closeModal);
+    }
+  }
+};
