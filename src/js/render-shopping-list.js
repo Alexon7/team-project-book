ed80dc4dff6dfdc5a527ec188cf2f8d066543187
Kralinskyi Ivan <i.kralinski@gmail.com>
2023-04-13 16:20:41 +0300

    update list

---
+++ src/js/render-shopping-list.js
@@ -0,0 +1,24 @@
+import { BookAPI } from './api-service';
+const bookApi = new BookAPI();
+
+const refs = {
+  emptyShoppingListEl: document.querySelector('.shopping-list__empty'),
+};
+
+console.log('REFS', refs.emptyShoppingListEl);
+
+async function renderShopppingList() {
+  const savedBookData = JSON.parse(localStorage.getItem('openInfoBook'));
+
+  if (!savedBookData) {
+    return;
+  }
+
+  refs.emptyShoppingListEl.classList.add('is-hidden');
+
+  const { data } = await bookApi.getBooksById(savedBookData._id);
+
+  console.log('BookInfo: ', data);
+}
+
+renderShopppingList();
