bd92b85b8c7adb780f0229025591def87303d56a
Ront31 <kolya.maslov.09@gmail.com>
2023-04-13 09:03:45 +0300

    Maslov a litle upd

--- src/js/render-description-book.js
+++ src/js/render-description-book.js
@@ -10,14 +10,35 @@ export function renderDescBook(aboutBook) {
      <div class="book-modal__book">
       <img
         class="book-modal__img"
-        alt="Заголовок"
+        src="${aboutBook.book_image ? aboutBook.book_image : ``}"
+        alt="${aboutBook.title}"
         loading="lazy"
        />
       <div class="book-modal__text">
-        <h4 class="title">Пример</h4>
-        <h4 class="author">Автор</h4>
-        <p class="description">Описание</p>
-        
+        <h4 class="title">${aboutBook.title}</h4>
+        <h4 class="author">${aboutBook.author}</h4>
+        <p class="description">${aboutBook.description}</p>
+
+        <div class="book-modal__links">
+          ${aboutBook.buy_links
+            .filter(
+              link =>
+                link.name === 'Amazon' ||
+                link.name === 'Bookshop' ||
+                link.name === 'Apple Books'
+            )
+            .map(link => {
+              return `<a class="link" href=${link.url}>
+              <img
+              class="img ${link.name}"
+              src="./img/${link.name}.png"
+              alt="Shop logo"
+
+            />
+            </a>`;
+            })
+            .join('')}
+        </div>
       </div>
      </div>
      <button class="button btnAdd active" type="button">Add to shopping list</button>
@@ -29,54 +50,3 @@ export function renderDescBook(aboutBook) {
   </div>
     `;
 }
-
-// export function renderDescBook(aboutBook) {
-//   return `
-//   <div class="book-modal">
-//      <svg class="close-icon" width="24" height="24">
-//              <use href="${sprite}#icon-close"></use>
-//       </svg>
-//     <div class="book-modal__content">
-//      <div class="book-modal__book">
-//       <img
-//         class="book-modal__img"
-//         src="${aboutBook.book_image ? aboutBook.book_image : ``}"
-//         alt="${aboutBook.title}"
-//         loading="lazy"
-//        />
-//       <div class="book-modal__text">
-//         <h4 class="title">${aboutBook.title}</h4>
-//         <h4 class="author">${aboutBook.author}</h4>
-//         <p class="description">${aboutBook.description}</p>
-
-//         <div class="book-modal__links">
-//           ${aboutBook.buy_links
-//             .filter(
-//               link =>
-//                 link.name === 'Amazon' ||
-//                 link.name === 'Bookshop' ||
-//                 link.name === 'Apple Books'
-//             )
-//             .map(link => {
-//               return `<a class="link" href=${link.url}>
-//               <img
-//               class="img ${link.name}"
-//               src="./img/${link.name}.png"
-//               alt="Shop logo"
-
-//             />
-//             </a>`;
-//             })
-//             .join('')}
-//         </div>
-//       </div>
-//      </div>
-//      <button class="button btnAdd active" type="button">Add to shopping list</button>
-//      <div class="btnRemove">
-//          <button class="button" type="button">Remove from shopping list</button>
-//      </div>
-
-//     </div>
-//   </div>
-//     `;
-// }
