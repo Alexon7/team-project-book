c5456245cb23667144f07480e2c576f6660bb559
Kralinskyi Ivan <i.kralinski@gmail.com>
2023-04-14 20:56:08 +0300

    new version

--- src/js/render-description-book.js
+++ src/js/render-description-book.js
@@ -84,3 +84,66 @@ export function renderDescBook(aboutBook) {
               alt="Shop logo"
             />
         </a> */
+
+// !========================================KRALINSKYI============================================================================
+// Перейменовано і переписано.
+/*
+        
+        export function renderBookDescription(bookData) {
+  const { image, title, author, description } = bookData;
+
+  return `
+  <div class="book-modal">
+     <svg class="close-icon" width="24" height="24">
+             <use href="../images/sprite.svg#close-btn"></use>
+      </svg>
+    <div class="book-modal__content">
+     <div class="book-modal__book">
+      <img
+      src=${book_image}
+        class="book-modal__img"
+        alt="${title}"
+        loading="lazy"
+      />
+      <div>
+        <div class="book-modal__text">
+          <h4 class="book-modal__title">${title}</h4>
+          <h4 class="book-modal__author">${author}</h4>
+          <p class="book-modal__description">${description}</p>
+        </div>
+        <div class="book-modal__links">
+          <a class="book-modal__link" href="">
+            <img
+              class="book-modal__link-image amazon"
+              src="../images/Amazon.png"
+              alt="Amazon logo"
+            />
+          </a>
+          <a class="book-modal__link" href="">
+            <img
+              class="book-modal__link-image bookshop"
+              src="../images/Bookshop.png"
+              alt="Bookshop logo"
+            />
+          </a>
+          <a class="book-modal__link" href="">
+            <img
+              class="book-modal__link-image apple-books"
+              src="../images/Apple Books.png"
+              alt="Apple Books logo"
+            />
+          </a>
+        </div>
+      </div>
+     </div>
+     <button class="book-modal__button btn-add active" type="button">Add to shopping list</button>
+     <div class="btn-remove-wrapper">
+         <button class="book-modal__button btn-remove" type="button">Remove from shopping list</button>
+         <p class="button-text">Congratulations! You have added the book to the shopping list. To delete, press the button "Remove from the shopping list".</p>
+     </div>
+    </div>
+  </div>
+  `;
+}
+
+        */
