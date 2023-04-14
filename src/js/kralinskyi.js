84ae3554f94c7f3283e7ee65c59b0df3bb5c02ea
Kralinskyi Ivan <i.kralinski@gmail.com>
2023-04-14 21:16:27 +0300

    repair

--- src/js/kralinskyi.js
+++ src/js/kralinskyi.js
@@ -1,22 +1,47 @@
-import { renderDescBook } from './render-description-book.js';
+// import { renderDescBook } from './render-description-book.js';
+// import { modal } from './modal.js';
+// import { BookAPI } from './api-service.js';
+// const bookApi = new BookAPI();
+
+// export async function showBookDescriptionModal(bookId) {
+//   try {
+//     const response = await bookApi.getBooksById(bookId);
+//     const bookData = await response.data;
+
+//     console.log('data - ', bookData);
+
+//     localStorage.setItem('openedBookData', JSON.stringify(bookData));
+//     const renderedInfoBook = renderDescBook(bookData);
+//     modal(renderedInfoBook);
+//   } catch (error) {
+//     console.log(error);
+//     return null;
+//   }
+// }
+
+// showBookDescriptionModal('643282b1e85766588626a086');
+
+import { renderBookDescription } from './render-description-book.js';
 import { modal } from './modal.js';
 import { BookAPI } from './api-service.js';
-const bookApi = new BookAPI();
+
+const bookAPI = new BookAPI();
+const addedBooks = JSON.parse(localStorage.getItem('addedBooks')) || [];
 
 export async function showBookDescriptionModal(bookId) {
   try {
-    const response = await bookApi.getBooksById(bookId);
-    const bookData = await response.data;
-
-    console.log('data - ', bookData);
-
+    const response = await bookAPI.getBookById(bookId);
+    const bookData = response.data;
     localStorage.setItem('openedBookData', JSON.stringify(bookData));
-    const renderedInfoBook = renderDescBook(bookData);
-    modal(renderedInfoBook);
+    const renderedBookDescription = renderBookDescription(bookData);
+    modal(renderedBookDescription, onAddBook);
   } catch (error) {
     console.log(error);
-    return null;
   }
 }
 
-showBookDescriptionModal('643282b1e85766588626a086');
+function onAddBook() {
+  const openedBookData = JSON.parse(localStorage.getItem('openedBookData'));
+  addedBooks.push(openedBookData);
+  localStorage.setItem('addedBooks', JSON.stringify(addedBooks));
+}
