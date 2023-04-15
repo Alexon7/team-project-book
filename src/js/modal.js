5b0c277d5861bae3ca8bf884a33083b71d90920c
Kralinskyi Ivan <i.kralinski@gmail.com>
2023-04-15 14:47:19 +0300

    start

--- src/js/modal.js
+++ src/js/modal.js
@@ -10,11 +10,18 @@ export const modal = async content => {
       instance.element().querySelector('.close-icon').onclick = instance.close;
       window.addEventListener('keydown', closeModal);
 
-      const btnAddBookToShoppingList = instance.element().querySelector('.btnAdd');
-      const btnRemoveBookFromShoppingList = instance.element().querySelector('.btnRemove');
+      const btnAddBookToShoppingList = instance
+        .element()
+        .querySelector('.btnAdd');
+      const btnRemoveBookFromShoppingList = instance
+        .element()
+        .querySelector('.btnRemove');
 
       btnAddBookToShoppingList.addEventListener('click', addBookToShoppingList);
-      btnRemoveBookFromShoppingList.addEventListener('click', removeBookFromShoppingList);
+      btnRemoveBookFromShoppingList.addEventListener(
+        'click',
+        removeBookFromShoppingList
+      );
     },
     onClose: () => {
       localStorage.removeItem('openInfoBook');
@@ -24,41 +31,63 @@ export const modal = async content => {
   });
   modal.show();
 
-
   function closeModal(event) {
     if (event.code === 'Escape') {
       modal.close();
     }
   }
 
-  
-
   function addBookToShoppingList() {
     try {
       console.log('add Book To Shopping List');
       infoBook = JSON.parse(localStorage.getItem('openInfoBook'));
-      localStorage.setItem(LS_KEY, JSON.stringify(infoBook)); 
+      localStorage.setItem(LS_KEY, JSON.stringify(infoBook));
       savedBooks.push(infoBook);
       console.log(savedBooks);
-
     } catch (error) {
-      console.error("Set state error: ", error.message);
+      console.error('Set state error: ', error.message);
     }
   }
-  
+
   function removeBookFromShoppingList() {
     try {
       console.log('remove Book From Shopping List');
       infoBook = JSON.parse(localStorage.getItem(LS_KEY));
       console.log(infoBook);
-      // localStorage.setItem(LS_KEY, JSON.stringify(infoBook)); 
+      // localStorage.setItem(LS_KEY, JSON.stringify(infoBook));
       // savedBooks.push(infoBook);
       // console.log(savedBooks);
-
     } catch (error) {
-      console.error("Set state error: ", error.message);
+      console.error('Set state error: ', error.message);
     }
-
   }
-  
 };
+
+// !============================MODAL.js=====================================
+/**   Цей код відповідає тільки за модалку, все.
+  
+ import * as basicLightbox from 'basiclightbox';
+import 'basiclightbox/dist/basiclightbox.min.css';
+
+export const modal = async content => {
+  const modal = basicLightbox.create(content, {
+    onShow: instance => {
+      instance.element().querySelector('.close').onclick = instance.close;
+    },
+    onClose: () => {
+      localStorage.removeItem('openInfoBook');
+      document.body.classList.remove('stop-scroll');
+    },
+  });
+  modal.show();
+  window.addEventListener('keydown', closeModal);
+
+  function closeModal(event) {
+    if (event.code === 'Escape') {
+      modal.close();
+      window.removeEventListener('keydown', closeModal);
+    }
+  }
+}; 
+ 
+ */
--- src/js/render-shopping-list.js
+++ src/js/render-shopping-list.js
@@ -1,33 +1,53 @@
-import { BookAPI } from './api-service';
-const bookApi = new BookAPI();
+import sprite from '../img/sprite.svg';
 
-const refs = {
-  emptyShoppingListEl: document.querySelector('.shopping-list__empty'),
-  notEmptyShoppingListEl: document.querySelector('.shopping-list__list'),
-};
+export function renderShoppingBooksList(savedBooks) {
+  const bookItemTemplate = book => `
+    <li class="shopping-book">
+      <button class="delete" type="button">
+        <svg class="icon" width="16" height="16">
+          <use href="${sprite}#icon-delete"></use>
+        </svg>
+      </button>
+      <div class="content">
+        <div class="book">
+          <img
+            class="cover"
+            src="${book.book_image ? book.book_image : ``}"
+            alt="${book.title}"
+            loading="lazy"
+            width="98"
+            height="139"
+          />
+          <div class="text">
+            <h4 class="title">${book.title}</h4>
+            <h4 class="type">${book.list_name}</h4>
+            <div class="links">
+              ${book.buy_links
+                .filter(
+                  link =>
+                    link.name === 'Amazon' ||
+                    link.name === 'Bookshop' ||
+                    link.name === 'Apple Books'
+                )
+                .map(link => {
+                  return `<a class="link" href=${link.url}>
+                    <img
+                      class="img ${link.name}"
+                      src="./img/${link.name}.png"
+                      alt="Shop logo"
+                    />
+                  </a>`;
+                })
+                .join('')}
+            </div>
+          </div>
+        </div>
+        <h4 class="author">${book.author}</h4>
+        <p class="description">${book.description}</p>
+      </div>
+    </li>`;
 
-// ! Показуємо добавлені в list книжки, якщо є. Якщо нема - пустий масив.
-const bookAddedToShopList = JSON.parse(localStorage.getItem('addedBooks'));
-if (!bookAddedToShopList)
-  localStorage.setItem('addedBooks', JSON.stringify([]));
-
-console.log('REFS', refs.emptyShoppingListEl);
-
-async function renderShopppingList() {
-  const savedBookData = JSON.parse(localStorage.getItem('openInfoBook'));
-
-  if (!savedBookData) {
-    return;
-  }
-
-  refs.notEmptyShoppingListEl.hasChildNodes &&
-    refs.emptyShoppingListEl.classList.toggle('is-hidden');
-
-  // refs.notEmptyShoppingListEl.classList.toggle('is-hidden');
-
-  const { data } = await bookApi.getBooksById(savedBookData._id);
-
-  console.log('BookInfo: ', data);
+  return savedBooks.length > 0
+    ? savedBooks.map(bookItemTemplate).join('')
+    : `<p>No books saved yet</p>`;
 }
-
-renderShopppingList();
