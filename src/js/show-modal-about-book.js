35d667e172432fa4817c4cacb524df0afb65d792
Ront31 <kolya.maslov.09@gmail.com>
2023-04-15 10:11:40 +0300

    a little update

--- src/js/show-modal-about-book.js
+++ src/js/show-modal-about-book.js
@@ -25,6 +25,40 @@ export const showBookModal = async bookId => {
   localStorage.setItem('openInfoBook', JSON.stringify(infoBook));
   const renderedInfoBook = renderDescBook(infoBook);
   modal(renderedInfoBook);
+
+  const showAdd = () => {
+    btnRemoveBookFromShoppingList.classList.remove('active');
+    btnAddBookToShoppingList.classList.add('active');
+  };
+  const showRemove = () => {
+    btnAddBookToShoppingList.classList.remove('active');
+    btnRemoveBookFromShoppingList.classList.add('active');
+  };
+  const isAddShoppingList = shoppingBooks.find(
+    book => book._id === openBook._id
+  );
+  isAddShoppingList ? showRemove() : showAdd();
+
+  btnAddBookToShoppingList.addEventListener('click', event => {
+    const openBook = JSON.parse(localStorage.getItem('openInfoBook'));
+    const shoppingBooks = JSON.parse(localStorage.getItem('shoppingList'));
+
+    const newShoppingBooks = shoppingBooks
+      ? [...shoppingBooks, openBook]
+      : [openBook];
+    localStorage.setItem('shoppingList', JSON.stringify(newShoppingBooks));
+    showRemove();
+  });
+  btnRemoveBookFromShoppingList.addEventListener('click', event => {
+    const openBook = JSON.parse(localStorage.getItem('openInfoBook'));
+    const shoppingBooks = JSON.parse(localStorage.getItem('shoppingList'));
+    const newShoppingBooks = shoppingBooks.filter(
+      book => book._id !== openBook._id
+    );
+    localStorage.setItem('shoppingList', JSON.stringify(newShoppingBooks));
+    showAdd();
+    renderDescBooks();
+  });
 };
 
 // export function show(event) {
