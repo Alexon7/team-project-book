388735e9a4b4553ede5131c6f2ec8b09dcb9898c
Irina Iskra <iskrairina.a@gmail.com>
2023-04-17 00:35:49 +0300

    swipper draft

--- src/js/render-support-Ukraine.js
+++ src/js/render-support-Ukraine.js
@@ -1,39 +1,91 @@
+import { arrayFunds } from './support-list';
+import Swiper from 'swiper';
 
- import { arrayFunds } from "./support-list";
+const supportListEl = document.querySelector('.support__list-js');
+const btnSwiperEl = document.querySelector('.swiper-button-next');
 
-const supportListEl = document.querySelector('.js_support_list');
-
-
-export const renderSupportList = items => {
-  const listItems = items
-    .map((item, index) => {
-      const { title, url, img1x, img2x } = item;
-      const number = (index + 1).toString().padStart(2, '0');
-      return `
-           <li class="support__item">
-              
-              <a
-                target="_blank"
-                rel="noopener noreferrer"
-                aria-label="company icon"
-                class="support__link"
-                href="${url}"
-              >
-              <p class="support__number">${number}</p>
-                <img
-                  src="/src/images/support-images/save@1x.png"
-                  alt="${title}"
-                  class="support__img"
-                  srcset="
-                  ${img1x} 1x,
-                  ${img2x} 2x
-                  "
-                />
-              </a>
-              `;
-    })
-    .join('');
-  supportListEl.innerHTML = `${listItems}`;
+let position = 0;
+const addLeadingZero = value => {
+  return String(value).padStart(2, '0');
 };
 
-renderSupportList(arrayFunds);
+const markupCardFund = (
+  { title, url, img1x, img2x },
+  position
+) => `<li class="support__list-item swiper-slide">
+  <p class="support__number">${position}</p>
+  <a class="support__link" href="${url}" target="_blank" rel="noopener noreferrer nofollow">
+    <img
+      srcset="${img1x} 1x, ${img2x} 2x"
+      src="${img1x}"
+      alt="${title}"
+      width="149"
+      loading="lazy"
+    />
+  </a>
+</li>`;
+
+const markupSetFunds = arrayFunds
+  .map((el, i) => {
+    position = addLeadingZero(i + 1);
+
+    return markupCardFund(el, position);
+  })
+  .join('');
+
+supportListEl.innerHTML = markupSetFunds;
+
+const swiper = new Swiper('.swiper', {
+  direction: 'vertical',
+  spaceBetween: 20,
+  slidesPerView: 'auto',
+  rewind: true,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+  },
+
+  plugins: {
+    scrollContainer: true,
+  },
+});
+
+swiper.update();
+
+btnSwiperEl.addEventListener('click', () => {
+  swiper.slideNext();
+});
+
+// export const renderSupportList = items => {
+//   const listItems = items
+//     .map((item, index) => {
+//       const { title, url, img1x, img2x } = item;
+//       const number = (index + 1).toString().padStart(2, '0');
+//       return `
+//            <li class="support__item">
+
+//               <a
+//                 target="_blank"
+//                 rel="noopener noreferrer"
+//                 aria-label="company icon"
+//                 class="support__link"
+//                 href="${url}"
+//               >
+//               <p class="support__number">${number}</p>
+//                 <img
+//                   src="/src/images/support-images/save@1x.png"
+//                   alt="${title}"
+//                   class="support__img"
+//                   srcset="
+//                   ${img1x} 1x,
+//                   ${img2x} 2x
+//                   "
+//                 />
+//               </a>
+//               `;
+//     })
+//     .join('');
+//   supportListEl.innerHTML = `${listItems}`;
+// };
+
+// renderSupportList(arrayFunds);
--- src/js/swiper.js
+++ src/js/swiper.js
@@ -15,35 +15,35 @@
 //   return swiper;
 // };
 
-import { renderSupportList } from './render-support-Ukraine';
-const imgSlides = document.querySelectorAll('.support__item');
-const sliderBtn = document.querySelector('.support__btn');
+// import { renderSupportList } from './render-support-Ukraine';
+// const imgSlides = document.querySelectorAll('.support__item');
+// const sliderBtn = document.querySelector('.support__btn');
 
-let counter = 0;
-let moveDown = false;
+// let counter = 0;
+// let moveDown = false;
 
-const verticalSlider = function () {
-  imgSlides.forEach(function (slide) {
-    slide.style.transform = translateY('-${counter * 100}%');
-  });
-};
+// const verticalSlider = function () {
+//   imgSlides.forEach(function (slide) {
+//     slide.style.transform = translateY('-${counter * 100}%');
+//   });
+// };
 
-sliderBtn.addEventListener('click', function () {
-  if (moveDown) {
-    counter -= 2;
-    if (counter < 0) {
-      counter = 0;
-      moveDown = false;
-      sliderBtn.querySelector('.support__icon').style.transform = '';
-    }
-  } else {
-    counter += 2;
-    if (counter >= imgSlides.length) {
-      counter = imgSlides.length - 1;
-      moveDown = true;
-      sliderBtn.querySelector('.support__icon').style.transform =
-        'rotate(180deg)';
-    }
-  }
-  verticalSlider(renderSupportList);
-});
+// sliderBtn.addEventListener('click', function () {
+//   if (moveDown) {
+//     counter -= 2;
+//     if (counter < 0) {
+//       counter = 0;
+//       moveDown = false;
+//       sliderBtn.querySelector('.support__icon').style.transform = '';
+//     }
+//   } else {
+//     counter += 2;
+//     if (counter >= imgSlides.length) {
+//       counter = imgSlides.length - 1;
+//       moveDown = true;
+//       sliderBtn.querySelector('.support__icon').style.transform =
+//         'rotate(180deg)';
+//     }
+//   }
+//   verticalSlider(renderSupportList);
+// });
