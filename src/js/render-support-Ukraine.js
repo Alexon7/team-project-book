4cab8a1b87f24bbdd320e6e5d254bc3ef9b7ea4c
jane05070 <jane05070@gmail.com>
2023-04-17 12:40:59 +0300

    cleaned up the code

--- src/js/render-support-Ukraine.js
+++ src/js/render-support-Ukraine.js
@@ -56,36 +56,4 @@ btnSwiperEl.addEventListener('click', () => {
   swiper.slideNext();
 });
 
-// export const renderSupportList = items => {
-//   const listItems = items
-//     .map((item, index) => {
-//       const { title, url, img1x, img2x } = item;
-//       const number = (index + 1).toString().padStart(2, '0');
-//       return `
-//            <li class="support__item">
 
-//               <a
-//                 target="_blank"
-//                 rel="noopener noreferrer"
-//                 aria-label="company icon"
-//                 class="support__link"
-//                 href="${url}"
-//               >
-//               <p class="support__number">${number}</p>
-//                 <img
-//                   src="/src/images/support-images/save@1x.png"
-//                   alt="${title}"
-//                   class="support__img"
-//                   srcset="
-//                   ${img1x} 1x,
-//                   ${img2x} 2x
-//                   "
-//                 />
-//               </a>
-//               `;
-//     })
-//     .join('');
-//   supportListEl.innerHTML = `${listItems}`;
-// };
-
-// renderSupportList(arrayFunds);
