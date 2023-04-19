2353470f7645caac2a2256ad718b8a2587828501
Alexon7 <alenkaalexon@gmail.com>
2023-04-19 23:36:24 +0200

    change footer

--- src/js/footer.js
+++ src/js/footer.js
@@ -5,12 +5,39 @@
 //   link.style.display = 'block';
 // });
 
+// const footerLink = document.querySelector('.heart');
+
+// footerLink.addEventListener('click', () => {
+//   const message = document.createElement('div');
+//   message.classList.add('message');
+//   message.textContent = 'Тисни, щоб подивитись код сторінки';
+//   document.body.appendChild(message);
+
+//   setTimeout(() => {
+//     message.style.opacity = 0;
+//     setTimeout(() => {
+//       message.remove();
+//     }, 1000);
+//   }, 1500);
+// });
+
 const footerLink = document.querySelector('.heart');
 
 footerLink.addEventListener('click', () => {
   const message = document.createElement('div');
   message.classList.add('message');
-  message.textContent = 'Тисни щоб подивитись код сторінки';
+
+  const messageText = document.createElement('span');
+  messageText.textContent = '';
+
+  const link = document.createElement('a');
+  link.classList.add('message-link');
+  link.href = 'https://github.com/Alexon7/team-project-book';
+  link.textContent = 'Тисни, щоб подивитись код сторінки';
+
+  message.appendChild(messageText);
+  message.appendChild(link);
+
   document.body.appendChild(message);
 
   setTimeout(() => {
